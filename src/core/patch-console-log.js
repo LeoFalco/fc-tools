02823f19b016d35f9d127c0b7503eeef60d77dee
Leo Falco <leonardo.falco@outlook.com>
2023-04-19 14:37:55 -0300

    chore: change logs

--- src/core/patch-console-log.js
+++ src/core/patch-console-log.js
@@ -4,15 +4,15 @@ const { blue, yellow, bgRed } = chalk
 
 const standardConsoleInfo = console.info
 console.info = (...args) => {
-  standardConsoleInfo(blue(' INFO '), ...args)
+  standardConsoleInfo(blue('INFO'), ...args)
 }
 
 const standardConsoleWarn = console.warn
 console.warn = (...args) => {
-  standardConsoleWarn(yellow(' WARN '), ...args)
+  standardConsoleWarn(yellow('WARN'), ...args)
 }
 
 const standardConsoleError = console.error
 console.error = (...args) => {
-  standardConsoleError(bgRed(' ERRO '), ...args)
+  standardConsoleError(bgRed('ERRO'), ...args)
 }
