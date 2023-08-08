0dfc00dedd3a5ac1ec62763dac9fb63c5142411b
Leo Falco <leonardo.falco@outlook.com>
2023-08-08 19:22:30 -0300

    feat: add rebase on clean

--- src/commands/repo-clean/index.js
+++ src/commands/repo-clean/index.js
@@ -23,20 +23,40 @@ class RepoCleanCommand {
     const currentBranchName = await $('git rev-parse --abbrev-ref HEAD')
     await $('git fetch --all --prune')
     await $('git remote prune origin')
-
     await $('git checkout master')
+    await $('git pull origin master')
 
-    const mergedBranches = await $('git branch --merged master')
+    const branches = await $('git branch')
       .then((output) => output.split('\n'))
       .then((branches) => branches.map((branch) => branch.trim()))
       .then((branches) => branches.filter((branch) => branch !== ''))
       .then((branches) => branches.filter((branch) => branch.startsWith('*') === false))
+      .then((branches) => branches.filter((branch) => branch !== 'master'))
+
+    for (const branch of branches) {
+      const isAhead = await $(`git log master..${branch} --oneline`).then((output) => output !== '')
+      if (isAhead) {
+        await $(`git checkout ${branch}`)
+        await $('git rebase master')
+        await $(`git push origin ${branch} --force --no-verify`)
+      } else {
+        console.info(`Branch ${branch} is not ahead of master`)
+      }
+    }
+
+    const mergedBranches = await $('git branch --merged master')
+      .then((output) => output.split('\n'))
+      .then((branches) => branches.map((branch) => branch.trim()))
+      .then((branches) => branches.map((branch) => branch.replace('origin/', '')))
+      .then((branches) => branches.filter((branch) => branch !== ''))
+      .then((branches) => branches.filter((branch) => branch.startsWith('*') === false))
+      .then((branches) => branches.filter((branch) => branch !== 'master'))
 
     if (mergedBranches.length === 0) {
-      console.log('No local branches to delete')
+      console.info('No local branches to delete')
     } else {
       for (const branch of mergedBranches) {
-        console.log(`Deleting local branch ${branch}`)
+        console.info(`Deleting local branch ${branch}`)
         await $(`git branch -D ${branch}`)
       }
     }
@@ -50,10 +70,11 @@ class RepoCleanCommand {
       .then((branches) => branches.map((branch) => branch.trim()))
       .then((branches) => branches.filter((branch) => branch !== ''))
       .then((branches) => branches.map((branch) => branch.replace('origin/', '')))
-      .then((branches) => branches.filter((branch) => branch.endsWith('master') === false))
+      .then((branches) => branches.filter((branch) => branch.startsWith('HEAD') === false))
+      .then((branches) => branches.filter((branch) => branch !== 'master'))
 
     if (mergedOnRemoteBranches.length === 0) {
-      console.log('No remote branches to delete')
+      console.info('No remote branches to delete')
     } else {
       const { remoteBranchesToDelete } = await inquirer.prompt([
         {
@@ -66,7 +87,7 @@ class RepoCleanCommand {
       ])
 
       for (const branch of remoteBranchesToDelete) {
-        console.log(`Deleting remote branch ${branch}`)
+        console.info(`Deleting remote branch ${branch}`)
         await $(`git push origin --delete ${branch} --no-verify`)
       }
     }
