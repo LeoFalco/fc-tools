f61dcf100f5149418583f62b6e88f366695bb9ef
Leo Falco <leonardo.falco@outlook.com>
2023-10-23 16:50:27 -0300

    feat: renomeia anbiente de homolog

--- src/commands/staging/index.js
+++ src/commands/staging/index.js
@@ -3,14 +3,14 @@ import { $ } from '../../core/exec.js'
 class PreviewCommand {
   install ({ program }) {
     program
-      .command('staging')
+      .command('homolog')
       .description('push current branch to preview environment')
-      .arguments('[preview_branch_name]', 'Branch or tag to delete, defaults to staging')
+      .arguments('[preview_branch_name]', 'Branch or tag to delete, defaults to homolog')
       .action(this.action.bind(this))
   }
 
   async action (previewBranchName) {
-    previewBranchName = previewBranchName || 'staging'
+    previewBranchName = previewBranchName || 'homolog'
     const previewTagExitCode = await $(`git rev-parse --verify ${previewBranchName}`, { returnProperty: 'exitCode', reject: false })
     if (previewTagExitCode === 0) {
       await $(`git tag -d ${previewBranchName}`, { reject: false })
