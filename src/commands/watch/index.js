23c23eeac881363ff0d6d649c7f4671a30199d67
Leo Falco <leonardo.falco@outlook.com>
2024-03-25 17:55:32 -0300

    feat: add get url retry

--- src/commands/watch/index.js
+++ src/commands/watch/index.js
@@ -1,3 +1,4 @@
+import { sleep } from 'openai/core.js'
 import { $ } from '../../core/exec.js'
 import open from 'open'
 
@@ -12,13 +13,30 @@ class PreviewCommand {
   async action () {
     const currentCommitSha = await $('git rev-parse HEAD')
 
-    const url = await $(`gh run list --commit ${currentCommitSha} --event push --json url --jq .[0].url`)
+    const url = await this.getUrl(currentCommitSha)
 
     await open(url, {
       wait: false
     })
 
     console.info(`Opening ${url} in browser`)
+  }
+
+  async getUrl (currentCommitSha, retries = 0) {
+    const url = await $(`gh run list --commit ${currentCommitSha} --event push --json url --jq .[0].url`)
+
+    if (url) {
+      return url
+    }
+
+    if (retries > 3) {
+      throw new Error('Failed to get url after 3 retries')
+    }
+
+    console.warn(`Failed to get url, retrying ${retries + 1}`)
+    await sleep(3000)
+
+    return this.getUrl(currentCommitSha, retries + 1)
   }
 }
 
