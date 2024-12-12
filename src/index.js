dbf272cd693bd7dfd598673d3e454f5af5489889
Leo Falco <leonardo.falco@outlook.com>
2024-12-12 17:48:34 -0300

    feat: fix paths

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import { $ } from './core/exec.js'
+import { fileURLToPath } from 'node:url'
+import { dirname } from 'node:path'
 
 async function run () {
-  await $('node src/update.js', {
+  const CURRENT_DIR = dirname(fileURLToPath(import.meta.url))
+
+  console.log('Current directory:', CURRENT_DIR)
+
+  await $(`node ${CURRENT_DIR}/src/update.js`, {
     stdio: 'inherit',
     loading: false
   })
 
   const args = process.argv.slice(2).join(' ')
 
-  const exitCode = await $(`node --no-warnings src/main.js ${args}`, {
+  const exitCode = await $(`node --no-warnings ${CURRENT_DIR}/src/main.js ${args}`, {
     stdio: 'inherit',
     reject: false,
     returnProperty: 'exitCode',
